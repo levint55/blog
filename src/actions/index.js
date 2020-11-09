75e43ec3bf0f5d6423add067451ac324224a8999
amabel <levintabel@gmail.com>
2020-11-09 13:05:26 +0700

    Refactor action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,23 @@
-import jsonPlaceholder from '../apis/JSONPlaceholder';
+import jsonPlaceholder from "../apis/JSONPlaceholder";
 
-export const fetchPosts = async () => {
-  return async (dispatch) => {
-    const response = await jsonPlaceholder.get('/posts');
+// Before Refactor
+// export const fetchPosts = async () => {
+//   return async (dispatch, getState) => {
+//     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({
-      type: 'FETCH_POST',
-      payload: response
-    })
-  }
+//     dispatch({
+//       type: 'FETCH_POST',
+//       payload: response
+//     })
+//   }
+// };
+
+// After Refactor
+export const fetchPosts = async () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({
+    type: "FETCH_POST",
+    payload: response,
+  });
 };
